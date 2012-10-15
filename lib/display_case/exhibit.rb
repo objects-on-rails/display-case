c8ec3ec0fa4fecad3d2470c5b2a17041d8177b9c
Sammy Larbi <sam@codeodor.com>
2012-10-15 10:42:59 -0500

    duplicate to_partial_path to avoid attempting to modify frozen String. Fixes #17

--- lib/display_case/exhibit.rb
+++ lib/display_case/exhibit.rb
@@ -92,7 +92,7 @@ module DisplayCase
 
     def to_partial_path
       if __getobj__.respond_to?(:to_partial_path)
-        __getobj__.to_partial_path
+        __getobj__.to_partial_path.dup
       else
         partialize_name(__getobj__.class.name)
       end
