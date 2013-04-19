ce8637d25095d8fc2ab22edcd6bf6279b1a56ccf
Sammy Larbi <sam@codeodor.com>
2013-04-19 07:01:20 -0500

    Add an implementation of a dumb cache to use as default

--- lib/display_case/cache/store.rb
+++ lib/display_case/cache/store.rb
@@ -1,6 +1,26 @@
 module DisplayCase
   module Cache
     class Store
+      def initialize
+        @store = {}
+      end
+      
+      def read(key)
+        @store[key]
+      end
+      alias :fetch :read
+      
+      def write(key, value)
+        @store[key] = value
+      end
+      
+      def delete(key)
+        @store.delete(key)
+      end
+      
+      def exist?(key)
+        @store.has_key?(key)
+      end
     end
   end
 end
--- spec/display_case/cache/store_spec.rb
+++ spec/display_case/cache/store_spec.rb
@@ -5,7 +5,30 @@ require_relative '../../fixtures/exhibits/blog_exhibit'
 module DisplayCase
   module Cache
     describe Store do
-      it "" do 
+      let(:key) { "key" }
+      let(:stored_value) { "value" }
+      let(:cache) { DisplayCase::Cache::Store.new }
+
+      it "can write and read an object" do 
+        assert_nil cache.read(key)
+        cache.write(key, stored_value)        
+        assert_equal stored_value, cache.read(key)
+      end
+      
+      it "can delete a key" do
+        cache.write(key, stored_value)        
+        assert_equal stored_value, cache.read(key)
+        cache.delete(key)
+        assert_nil cache.read(key)
+      end
+      
+      it "can fetch a key" do 
+        cache.write(key, stored_value)        
+        assert_equal stored_value, cache.fetch(key)
+      end
+      
+      it "can tell if a key exists" do 
+        assert !cache.exist?(key)
       end
     end
   end
