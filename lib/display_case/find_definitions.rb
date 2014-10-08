45785f4ee78accc2d5fd77b2c375a7e476b9c30a
Sammy Larbi <sam@codeodor.com>
2014-10-08 09:45:43 -0500

    Only reload exhibits that have changed (fixes #50)

--- lib/display_case/find_definitions.rb
+++ lib/display_case/find_definitions.rb
@@ -4,13 +4,23 @@ module DisplayCase
     absolute_definition_file_paths = configuration.definition_file_paths.map {|path| File.expand_path(path) }
 
     absolute_definition_file_paths.uniq.each do |path|
-      load("#{path}.rb") if File.exists?("#{path}.rb")
+      file = "#{path}.rb"
+      display_case_load file
 
       if File.directory? path
         Dir[File.join(path, '**', '*.rb')].sort.each do |file|
-          load file
+          display_case_load file
         end
       end
+    end
+  end
+
+  private
+  def self.display_case_load(file)
+    @file_changes ||= {}
+    if File.exists?(file) && (@file_changes[file].to_i < (mtime = File.mtime(file).to_i))
+      load file
+      @file_changes[file] = mtime
     end
   end
 end
